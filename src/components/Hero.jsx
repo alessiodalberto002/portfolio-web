4b1147b7a4c6f6730495732130ab5e4438c7f3a9
alessiodalberto002 <aledalberto002.com>
2026-05-12 22:34:35 +0200

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+const Hero = () => {
+
+    const blueStyle = { color: "#0066FF" };
+
+    return (
+
+        <div className="vh-100 d-flex flex-column gap-5 p-5" style={{ maxWidth: "980px", fontFamily: "Switzer-Regular" }}>
+            <h1 className="m-0 p-0" style={blueStyle}>
+                Ciao a tutt*,
+            </h1>
+
+            <h2 className="m-0 p-0">
+                sono <span style={blueStyle}>Alessio,</span> disegno cose sul web e non solo. <br />
+                Mi è stato consigliato di creare una "vetrina" per i miei lavori e così ho fatto.
+            </h2>
+
+            <h3 className="m-0 p-0">
+                Qui trovi un <a href="" className="text-decoration-underline" style={blueStyle}>link</a> per i miei lavori. <br />
+                Per bere un caffè insieme o per un saluto scrivi a: <a href="mailto:alessiodalbertodesign@gmail.com" className="text-decoration-underline" style={blueStyle}>alessiodalbertodesign@gmail.com</a>
+            </h3>
+
+            <div className="">
+                <h4 className="m-0 p-0">Per continuare a giudicarmi potete visitare questi profili:</h4>
+                <div className="d-flex gap-3">
+                    <a href="https://linkedin.com" target="_blank" className="text-dark fs-3"><i style={blueStyle} className="fa-brands fa-linkedin-in"></i></a>
+                    <a href="https://linkedin.com" target="_blank" className="text-dark fs-3"><i style={blueStyle} className="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+
+}
+
+export default Hero
